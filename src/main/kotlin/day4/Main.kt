c1c496585d36dd2c31ae71c1d75b7f3e88e251bb
jgosling <jgosling195@gmail.com>
2023-11-22 22:11:08 +0000

    Day 4 part 1 complete

--- src/main/kotlin/day4/Main.kt
+++ src/main/kotlin/day4/Main.kt
@@ -2,50 +2,33 @@ package day4
 
 import java.io.File
 
-fun main() {
-    val input = File("inputs/day4.txt").readText().split("\n")
-    var id = "None"
-    val minutesAsleep = HashMap<Int, String>() // minutes, ids
-    var shiftStart = 0
-    var startHour = 0
-    input.forEach {
-        println(it)
-        val text = it.split("] ")[1]
-        val hour = it.split("] ")[0].split(" ")[1].split(":")[0].toInt()
-        val minute = it.split("] ")[0].split(" ")[1].split(":")[1].toInt()
+fun solvePart1(sleepMinutesPerGuard: MutableMap<Int, List<Int>>): Int {
+    return sleepMinutesPerGuard
+        .maxBy { it.value.size }
+        .run { key * value.groupBy { it }.maxBy { it.value.size }.key }
+}
 
-        if ("begins shift" in text) {
-            id = text.split("#")[1].split(" begins")[0]
-            if (hour == 0) {
-                if (minute in minutesAsleep) {
-                    minutesAsleep[minute] = minutesAsleep[minute] + " $id"
-                } else {
-                    minutesAsleep[minute] = id
-                }
-            }
-            shiftStart = minute
-            startHour = hour
-        } else if ("wakes up" in text) {
-            if (minute in minutesAsleep) {
-                minutesAsleep[minute] = minutesAsleep[minute] + " $id"
-            } else {
-                minutesAsleep[minute] = id
-            }
-            shiftStart = minute
-        } else if ("falls asleep" in text) {
-            if (startHour != 0) {
-                shiftStart = -1
-            }
-                for (i in shiftStart + 1..<minute) {
-                    if (i in minutesAsleep) {
-                        minutesAsleep[i] = minutesAsleep[i] + " $id"
-                    } else {
-                        minutesAsleep[i] = id
-                    }
-                }
-        } else if ("wakes up" in text) {
+fun parse(input: List<String>): MutableMap<Int, List<Int>> {
+    val sleeps = mutableMapOf<Int, List<Int>>()
+    var guard = 0
+    var sleepStart = 0
 
+    input.sorted().forEach { row ->
+        val minute = row.split("] ")[0].split(" ")[1].split(":")[1].toInt()
+        when {
+            row.contains("Guard") -> guard = row.split("#")[1].split(" begins")[0].toInt()
+            row.contains("asleep") -> sleepStart = minute
+            else -> {
+                val sleepMinutes = (sleepStart until minute).toList()
+                sleeps.merge(guard, sleepMinutes) { a, b -> a + b }
+            }
         }
     }
-    println("complete")
+    return sleeps
+}
+
+fun main() {
+    val input = File("inputs/day4.txt").readText().split("\n")
+    val sleeps = parse(input)
+    println(solvePart1(sleeps))
 }
